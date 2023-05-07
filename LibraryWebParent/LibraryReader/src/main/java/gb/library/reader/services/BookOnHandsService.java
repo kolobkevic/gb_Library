dea1f166147d3dbf986f98e527cdb98c19cb0ff6
kolobkevic <kolobkevic@gmail.com>
2023-05-07 10:27:02 +0300

    Добавлена возможность просмотра читателем кнги на руках с отображением даты возврата книги

--- LibraryWebParent/LibraryReader/src/main/java/gb/library/reader/services/BookOnHandsService.java
+++ LibraryWebParent/LibraryReader/src/main/java/gb/library/reader/services/BookOnHandsService.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 
 @Service("readerBookOnHandsService")
 @RequiredArgsConstructor
