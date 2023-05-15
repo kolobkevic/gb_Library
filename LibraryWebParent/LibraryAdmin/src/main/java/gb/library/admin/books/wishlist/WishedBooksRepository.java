712b84e57afa56fffc036ae3e798213c282ea60d
kolobkevic <kolobkevic@gmail.com>
2023-05-15 21:03:22 +0300

    Удалил рест контроллер. Удалил метод checkUnique.

--- LibraryWebParent/LibraryAdmin/src/main/java/gb/library/admin/books/wishlist/WishedBooksRepository.java
+++ LibraryWebParent/LibraryAdmin/src/main/java/gb/library/admin/books/wishlist/WishedBooksRepository.java
@@ -1,7 +1,6 @@
 package gb.library.admin.books.wishlist;
 
 import gb.library.admin.utils.paging.SearchRepository;
-import gb.library.common.entities.Author;
 import gb.library.common.entities.BooksWishlist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,8 +12,5 @@ public interface WishedBooksRepository extends SearchRepository<BooksWishlist, I
     @Query("SELECT b FROM BooksWishlist b WHERE b.book.title LIKE %?1% or b.book.author.firstName LIKE %?1%"
             + " OR b.book.author.lastName LIKE %?1% OR b.book.genre.name LIKE %?1%")
     Page<BooksWishlist> findAll(String keyword, Pageable pageable);
-
-    @Query("select b from BooksWishlist b where b.book.title = ?1 and b.book.author = ?2")
-    BooksWishlist findByTitleAndAuthor(String title, Author author);
 
 }
