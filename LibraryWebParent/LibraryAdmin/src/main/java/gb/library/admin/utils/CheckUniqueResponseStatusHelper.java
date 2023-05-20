139bbf2ba440a72e66ff52e2595bef16d320d282
Ilya <petermoloch@gmail.com>
2023-05-20 13:17:05 +0300

    Работа с пользовательскими ролями в админке + тесты

--- LibraryWebParent/LibraryAdmin/src/main/java/gb/library/admin/utils/CheckUniqueResponseStatusHelper.java
+++ LibraryWebParent/LibraryAdmin/src/main/java/gb/library/admin/utils/CheckUniqueResponseStatusHelper.java
@@ -8,7 +8,9 @@ public class CheckUniqueResponseStatusHelper {
     public static final String OK = "OK";
     public static final String DUPLICATED = "Duplicated";
 
-    public static String getCheckStatus(IdBasedEntity obj, int id){
+    public static final String ERROR = "Checking Error";
+
+    public static String getCheckStatus(IdBasedEntity obj, Integer id){
         if (obj != null) {
             if (!Objects.equals(obj.getId(), id)) {
                 return DUPLICATED;
