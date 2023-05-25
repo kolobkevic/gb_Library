01bf03894bb99abdd5aae2fa0b879674a97fb9ef
Ilya <petermoloch@gmail.com>
2023-05-25 17:07:32 +0300

    небольшие изменения

--- LibraryWebParent/LibraryReadersPersonalData/src/main/resources/db/migration/V1__init_db.sql
+++ LibraryWebParent/LibraryReadersPersonalData/src/main/resources/db/migration/V1__init_db.sql
@@ -6,18 +6,19 @@ CREATE TABLE "readers" (
     "id"        BIGSERIAL       PRIMARY KEY,-- Идентификатор записи
     "reader_id" BIGINT          NOT NULL,   -- Уникальный идентификатор читателя
 
-    "family"    VARCHAR(64)     NOT NULL,   -- Фамилия
+    "family"    VARCHAR(64)     NOT NULL,   -- Фамилия  //УДАЛИТЬ
     "name"      VARCHAR(64)     NOT NULL,   -- Имя
-    "surname"   VARCHAR(64)     NULL,       -- Отчество
+    "surname"   VARCHAR(64)     NULL,       -- Фамилия
 
     "birthday"  DATE            NULL,       -- Дата рождения
 
     "phone1"    VARCHAR(20)     NULL,       -- Телефон (домашний)
     "phone2"    VARCHAR(20)     NULL,       -- Телефон (рабочий)
-    "email"     VARCHAR(20)     NULL,       -- Адрес электронной почты
+    "email"     VARCHAR(50)     NULL,       -- Адрес электронной почты
 
     "address"   VARCHAR(256)    NULL,       -- Адрес места жительства
+    "passport"  VARCHAR(30)     NULL        -- Паспорт/личный номер
 
     CONSTRAINT "reader_id_unique" UNIQUE ("reader_id"),
-    CONSTRAINT "main_info_unique" UNIQUE ("family", "name", "surname", "birthday")
+    CONSTRAINT "main_info_unique" UNIQUE ("name", "surname", "birthday")
 );
