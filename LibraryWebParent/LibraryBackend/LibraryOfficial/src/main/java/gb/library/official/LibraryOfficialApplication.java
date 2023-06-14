ad354f517d78301682041e5a33e287d97729390e
Ilya <petermoloch@gmail.com>
2023-06-14 12:23:47 +0300

    Authors / Storages / World Books: валидаторы и конверторы

--- LibraryWebParent/LibraryBackend/LibraryOfficial/src/main/java/gb/library/official/LibraryOfficialApplication.java
+++ LibraryWebParent/LibraryBackend/LibraryOfficial/src/main/java/gb/library/official/LibraryOfficialApplication.java
@@ -2,7 +2,6 @@ package gb.library.official;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
