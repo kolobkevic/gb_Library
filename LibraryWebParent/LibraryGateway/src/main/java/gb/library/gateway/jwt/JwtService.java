8a6146419ebd0ece36c87333a402dc02dc408843
kolobkevic <kolobkevic@gmail.com>
2023-06-18 16:34:02 +0300

    Gateway ready. Исправлена навигация на фронте читателей и сотрудников.

--- LibraryWebParent/LibraryGateway/src/main/java/gb/library/gateway/jwt/JwtService.java
+++ LibraryWebParent/LibraryGateway/src/main/java/gb/library/gateway/jwt/JwtService.java
@@ -1,7 +1,6 @@
 package gb.library.gateway.jwt;
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
